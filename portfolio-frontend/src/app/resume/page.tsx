19a4c19fc1f5da61d6f943959f67d73fa3f5971c
DaniKaizenNetwork <danielitasanabria10@gmail.com>
2025-05-20 16:11:50 -0500

    restaurar

--- portfolio-frontend/src/app/resume/page.tsx
+++
@@ -1,8 +0,0 @@
-import Layout from '@/modules/layouts/layout'
-import React from 'react'
-
-export default function PageResume() {
-  return (
-    <Layout>Page Resumen</Layout>
-  )
-}
--- portfolio-frontend/src/services/axios.service.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-
-export const axiosClient = axios.create({
-    baseURL: process.env.BACKEND_URL || 'http://localhost:4000/api/v1',
-    headers:{
-        "Content-Type": 'application/json'
-    }
-})
